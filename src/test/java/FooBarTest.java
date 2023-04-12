6aee06a34dd040285365a602d65d6bc7fce345fa
BlonRed <balf,dw@gmail.com>
2023-04-12 21:40:30 +0400

    Optimization of testing method

--- src/test/java/FooBarTest.java
+++ src/test/java/FooBarTest.java
@@ -1,27 +1,34 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FooBarTest {
     static FooBar fB;
+    private ByteArrayOutputStream outputStream;
     final int n = 15;
     final String s = System.lineSeparator();
     final String expectedOutput = "1" + s + "2" + s + "Foo" + s + "4" + s + "Bar" + s + "Foo"
             + s + "7" + s + "8" + s + "Foo" + s + "Bar" + s + "11" + s + "Foo" + s + "13"
             + s + "14" + s + "FooBar" + s;
 
+    void recordTextFromConsole() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
     @BeforeAll
-    static void prepare(){
+    static void prepare() {
         fB = new FooBar();
     }
 
     @Test
     void testFooBarVersion1() {
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        recordTextFromConsole();
 
         fB.fooBarVersion1(n);
 
@@ -31,8 +38,7 @@ class FooBarTest {
     @Test
     void testFooBarVersion2() {
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        recordTextFromConsole();
 
         fB.fooBarVersion2(n);
 
@@ -42,8 +48,7 @@ class FooBarTest {
     @Test
     void testFooBarVersion3() {
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        recordTextFromConsole();
 
         fB.fooBarVersion3(n);
 
@@ -53,8 +58,7 @@ class FooBarTest {
     @Test
     void testFooBarVersion4() {
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        recordTextFromConsole();
 
         fB.fooBarVersion4(n);
 
